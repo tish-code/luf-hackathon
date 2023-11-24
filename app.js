431341fe685ec01a84888c04e0ed028b5a505207
tish-code <eliastish2003@gmail.com>
2023-11-24 06:42:05 +0100

    implemented modal overlays

--- app.js
+++ app.js
@@ -1,2 +1,40 @@
-const userModal = document.getElementById("user-modal");
-console.log(userModal);
+const userModal = document.querySelector(".user-modal-overlay");
+const authUser = document.querySelector(".user");
+const alertModal = document.querySelector(".alerts-overlay");
+const notificationIcon = document.querySelector(".notification");
+
+function toggleUserModal() {
+  if (userModal.style.display === "" || userModal.style.display === "none") {
+    userModal.style.display = "block";
+  } else {
+    userModal.style.display = "none";
+  }
+}
+function toggleAlertModal() {
+  if (alertModal.style.display === "" || alertModal.style.display === "none") {
+    alertModal.style.display = "block";
+  } else {
+    alertModal.style.display = "none";
+  }
+}
+
+authUser.addEventListener("click", toggleUserModal);
+notificationIcon.addEventListener("click", toggleAlertModal);
+
+alertModal.addEventListener("click", (e) => {
+  console.log(e.target);
+  if (e.target === e.currentTarget) {
+    if (alertModal.style.display === "block") {
+      alertModal.style.display = "none";
+    }
+  }
+});
+
+userModal.addEventListener("click", (e) => {
+  console.log(e.target);
+  if (e.target === e.currentTarget) {
+    if (userModal.style.display === "block") {
+      userModal.style.display = "none";
+    }
+  }
+});
